dd882e5d8291c333a618102141d0637dc8dd4881
Talha <Talha@DESKTOP-A897KH8>
2026-08-24 19:06:09 +0300

    Proje dosyası ekle.

---
+++ project/Menu/imgui_menu.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "../ImGui/imgui.h"
+#include "../ImGui/imgui_internal.h"
+#include "../ImGui/backends/imgui_impl_dx11.h"
+#include "../ImGui/backends/imgui_impl_win32.h"
+#include "../offsets.hpp"
+#include <d3d11.h>
+#include <dxgi.h>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
+extern void save_config();
+extern void load_config();
+extern void set_current_config(const std::string& name);
+extern std::string get_current_config();
+extern std::vector<std::string> list_configs();
